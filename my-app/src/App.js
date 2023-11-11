3b19c16b77336e9d6764570b83b56291b0529905
abranava <abranava@amazon.com>
2023-11-10 21:16:14 -0700

    Created React App

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,45 +1,87 @@
-import React, { useState, useEffect } from "react";
-import "./App.css";
-import Main from "./containers/Main";
-import { ThemeProvider } from "styled-components";
-import { themes } from "./theme";
-import { GlobalStyles } from "./global";
-import { CursorProvider } from "react-cursor-custom";
-import { settings } from "./portfolio";
-import ReactGA from "react-ga";
+import React from 'react';
+import {
+  ChakraProvider,
+  Box,
+  Text,
+  Link,
+  VStack,
+  Grid,
+  Heading,
+  Image,
+  SimpleGrid,
+  Flex,
+  Input,
+  Button,
+  theme,
+} from '@chakra-ui/react';
+import { ColorModeSwitcher } from './ColorModeSwitcher';
+import { Logo } from './Logo';
 
 function App() {
-  useEffect(() => {
-    if (settings.googleTrackingID) {
-      ReactGA.initialize(settings.googleTrackingID, {
-        testMode: process.env.NODE_ENV === "test",
-      });
-      ReactGA.pageview(window.location.pathname + window.location.search);
-    }
-  }, []);
-
-  const [theme, setTheme] = useState(localStorage.getItem("theme") || "dark");
-  const useCursor = settings.useCustomCursor;
-
   return (
-    <ThemeProvider theme={themes[theme]}>
-      <>
-        <GlobalStyles />
-        <div>
-          {useCursor ? (
-            <CursorProvider
-              color={themes[theme].secondaryText}
-              ringSize={25}
-              transitionTime={75}
-            >
-              <Main theme={themes[theme]} setTheme={setTheme} />
-            </CursorProvider>
-          ) : (
-            <Main theme={themes[theme]} setTheme={setTheme} />
-          )}
-        </div>
-      </>
-    </ThemeProvider>
+    <ChakraProvider theme={theme}>
+      <Box textAlign="center" fontSize="xl">
+        <Grid minH="100vh" p={3}>
+          <ColorModeSwitcher justifySelf="flex-end" />
+
+          {/* Header Section */}
+          <VStack spacing={8}>
+            <Logo h="40vmin" pointerEvents="none" />
+            <Heading>Abraham Navarro</Heading>
+            <Text>Frontend Developer & Designer</Text>
+          </VStack>
+
+          {/* About Section */}
+          <Box p={5}>
+            <Heading>About Me</Heading>
+            <Text mt={3}>
+            Bilingual operational leader with a track record in data analysis and process innovation, notably developing FRASER, an AI bot for Amazon. Experienced in creating and managing Amazon's Business Intelligence branch, significantly enhancing efficiency through automation. Skilled in diverse technical tools, I'm adept at driving operational improvements and adapting swiftly to new technologies.            </Text>
+          </Box>
+
+          {/* Portfolio Section */}
+          <Box p={5}>
+            <Heading>My Portfolio</Heading>
+            <SimpleGrid columns={2} spacing={10} mt={5}>
+              {/* Repeat this Box for each project */}
+              <Box boxShadow="lg" p={5} rounded="md">
+                <Image src="/project-image.jpg" alt="Project Image" />
+                <Text mt={3}>Project Description</Text>
+              </Box>
+              {/* Repeat this Box for each project */}
+              <Box boxShadow="lg" p={5} rounded="md">
+                <Image src="/project-image.jpg" alt="Project Image" />
+                <Text mt={3}>Project Description</Text>
+              </Box>
+              {/* Repeat this Box for each project */}
+              <Box boxShadow="lg" p={5} rounded="md">
+                <Image src="/project-image.jpg" alt="Project Image" />
+                <Text mt={3}>Project Description</Text>
+              </Box>
+              {/* Repeat this Box for each project */}
+              <Box boxShadow="lg" p={5} rounded="md">
+                <Image src="/project-image.jpg" alt="Project Image" />
+                <Text mt={3}>Project Description</Text>
+              </Box>
+            </SimpleGrid>
+          </Box>
+          
+
+          {/* Contact Section */}
+          <Flex justify="center" p={5}>
+            <VStack spacing={5}>
+              <Heading>Contact Me</Heading>
+              <Input placeholder="Your Email" />
+              <Button colorScheme="teal">Send Message</Button>
+            </VStack>
+          </Flex>
+
+          {/* Footer Section */}
+          <Box p={5}>
+            <Text>&copy; {new Date().getFullYear()} Abraham Navarro. All rights reserved.</Text>
+          </Box>
+        </Grid>
+      </Box>
+    </ChakraProvider>
   );
 }
 
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,28 +1,26 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BaseProvider, LightTheme } from "baseui";
-import { Provider as StyletronProvider } from "styletron-react";
-import { Client as Styletron } from "styletron-engine-atomic";
+import { ColorModeScript } from '@chakra-ui/react';
+import React, { StrictMode } from 'react';
+import * as ReactDOM from 'react-dom/client';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import * as serviceWorker from './serviceWorker';
 
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
 
-import "./index.css";
-// import "bootstrap/dist/css/bootstrap.min.css";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
-import "./assests/font-awesome/css/all.css";
-
-const engine = new Styletron();
-
-ReactDOM.render(
-	<StyletronProvider value={engine}>
-		<BaseProvider theme={LightTheme}>
-				<App />
-	</BaseProvider>
-	</StyletronProvider>,
-	document.getElementById("root")
+root.render(
+  <StrictMode>
+    <ColorModeScript />
+    <App />
+  </StrictMode>
 );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+// Learn more about service workers: https://cra.link/PWA
 serviceWorker.unregister();
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
